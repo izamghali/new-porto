44a7f7cf3bc8dd9ee9290202dde2b93c9a850fc0
Izam Ghali <izamghali18@gmail.com>
2024-07-28 09:11:26 +0700

    fix: added type on constant.ts & removed project dropdown

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -14,105 +14,72 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import SearchBar from "./SearchBar"
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "WearDrobe",
-    href: "https://jcwd030801.purwadhikabootcamp.com/",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Samiya Studio",
-    href: "https://samiyastudio.com/",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Mixin' Jam",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Wham Bam",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Ridgeline Prototype",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
+import { PiRocket } from "react-icons/pi"
+import useData from "@/app/projects/useData"
 
 export function Header() {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList className="flex gap-8">
-        <NavigationMenuItem className="font-bold text-md">
-            <Link href={'/'}>Home</Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="font-bold text-md">
-            <SearchBar placeholder={"Search project"} />
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        {/* <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem> */}
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
+
+    const { projectInfo } = useData()
+
+    return (
+        <NavigationMenu>
+            <NavigationMenuList className="flex lg:gap-8 gap-4 max-xs:px-4 max-xs:justify-between justify-center max-xs:w-screen">
+                <NavigationMenuItem className="font-bold text-md">
+                    <Link href={'/'}>Home</Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="font-bold text-md">
+                    <SearchBar placeholder={"Search project"} />
+                </NavigationMenuItem>
+                {/* <NavigationMenuItem>
+                    <NavigationMenuTrigger className="flex gap-2"><span className="max-sm:hidden">Projects</span><PiRocket /></NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                        {projectInfo.map((item) => (
+                            <ListItem
+                            key={item.title}
+                            title={item.title}
+                            href={item.href}
+                            >
+                            {item.desc}
+                            </ListItem>
+                        ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem> */}
+                {/* <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Documentation
+                    </NavigationMenuLink>
+                </Link>
+                </NavigationMenuItem> */}
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
+    return (
+        <li>
+        <NavigationMenuLink asChild>
+            <a
+            ref={ref}
+            className={cn(
+                "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                className
+            )}
+            {...props}
+            >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                {children}
+            </p>
+            </a>
+        </NavigationMenuLink>
+        </li>
+    )
 })
 ListItem.displayName = "ListItem"
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,11 +1,38 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, ReactNode } from "react";
+
+export enum ProjectType {
+    "Team Project",
+    "Hobby Project",
+    "On Progress",
+    "Work-related Project"
+}
+
+export interface ProjectMember {
+    name: string,
+    portfolio: string
+}
+
+export interface ProjectTool {
+    name: string,
+    href: string,
+    logo: ReactNode
+}
+
+export interface ProjectLink {
+    name: string,
+    link: string
+}
 
 export interface ProjectInfo {
     title: string,
     desc: string, 
     imgUrl: string,
     href: string, 
-    tools: ReactElement, 
+    links?: ProjectLink[],
+    tools: ProjectTool[], 
     tags?: ReactElement, 
     content?: ReactElement,
+    type?: ProjectType,
+    year?: number,
+    members?: ProjectMember[]
 }
